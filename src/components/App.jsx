5d8a43d282e4563a43139bacda9e367c42595060
KarpenkoOleksandr <131707071+KarpenkoOleksandr@users.noreply.github.com>
2023-11-19 19:58:06 +0200

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import { ContactForm } from "./ContactForm/ContactForm";
+import {ContactList} from "./ContactList/ContactList"
+import {Filter} from "./Filter/Filter"
+import { Container, Title } from './App.styled'
+import { GlobalStyle } from "GlobalStyle";
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: ''
+  }
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parseContacts = JSON.parse(contacts);
+    if (parseContacts) {
+      this.setState({contacts: parseContacts})
+    }
+  }
+
+  componentDidUpdate(prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+    }
+  }
+
+  addContact = (newContact) => {
+    this.setState((prevState) => ({
+      contacts: [...prevState.contacts, newContact]
+    }));
+  };
+
+  deleteContact = (id) => {
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter((contact) => contact.id !== id)
+    }));
+  };
+
+  setFilter = (filterValue) => {
+    this.setState({
+      filter: filterValue
+    });
+  };
+
+  filteredContacts = () => {
+    const { contacts, filter } = this.state;
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  }
+    render() {
+    const { contacts, filter } = this.state;
+
+    return (
+      <Container>
+        <Title>Phonebook</Title>
+        <ContactForm addContact={this.addContact} contacts={contacts} />
+
+        <Title>Contacts</Title>
+        <Filter filter={filter} setFilter={this.setFilter} />
+        <ContactList contacts={this.filteredContacts()} deleteContact={this.deleteContact} />
+        <GlobalStyle />
+      </Container>
+    );
+  }
+}
